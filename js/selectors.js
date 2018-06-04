e42a70753ab1ebab51de62028bb04bafac85b42d
Edward Mixon <edwardchase@gatech.edu>
2018-06-04 21:12:26 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,23 @@
-// declare your functions here...
+function paragraphSelector() {
+  var p = $('p')
+  return p;
+}
+
+function lastImageSelector() {
+  var last = $('img:last');
+  return last;
+}
+
+function ninjaBabySelector() {
+  var baby = $('#baby-ninja');
+  return baby;
+}
+
+function divSelector() {
+  var divs = $('div .pics');
+  return divs;
+}
+
+function firstListItem() {
+  var first = $('ul #pic-list:first-child');
+}
